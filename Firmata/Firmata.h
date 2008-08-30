70e2853fb8efb54a6ccb9476f0585816c182a792
eighthave <eighthave@5a7bb8b8-8b4a-0410-973d-494203d638bc>
2008-08-30 16:22:46 +0000

    switch license of the library to LGPL and removed unused variable to get rid of a compile warning

--- Firmata/Firmata.h
+++ Firmata/Firmata.h
@@ -1,11 +1,11 @@
 /*
   Firmata.h - Firmata library
-  Copyright (c) 2007-2008 Free Software Foundation.  All rights reserved.
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+  Copyright (C) 2006-2008 Hans-Christoph Steiner.  All rights reserved.
+ 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
 
   See file LICENSE.txt for further informations on licensing terms.
 */
